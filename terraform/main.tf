8b5a0608b22de70dedd7cbf03ab9f07d164d66ec
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-06-28 11:19:58 +0000

    Cleaned syntax and re-structured main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,73 +1,23 @@
 terraform {
     required_providers {
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "~> 3.0.2"
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
         }
     }
+
+    required_version = ">= 1.3.0"
 }
 
-provider "docker" {
-    host = "unix://var/run/docker.sock"
+provider "aws" {
+    region = "ap-southeast-2" 
 }
 
-# Image definitions 
-
-resource "docker_image" "juice_shop" {
-    name = var.juice_shop_image
-}
-
-resource "docker_image" "zap" {
-    name = var.zap_image
-}
-
-resource "docker_image" "bandit" {
-    name = var.bandit_image
-}
-
-resource "docker_image" "trivy" {
-    name = var.trivy_image
-}
-
-# Container definitions
-
-# Juice Shop container
-resource "docker_container" "juice_shop" {
-    name = "juice-shop"
-    image = docker_image.juice_shop.name
-
-        ports {
-        internal = var.juice_shop_port
-        external = var.juice_shop_port
-    }
-}
-
-# Zap container
-resource "docker_container" "zap" {
-    name = "zap"
-    image = docker_image.zap.name
-
-    ports {
-        internal = var.zap_port
-        external = var.zap_port
-    }
-}
-
-# Bandit container (no ports usually needed)
-resource "docker_container" "bandit" {
-    name = "bandit"
-    image = docker_image.bandit.name
-}
-
-# Trivy container (no ports usually needed)
-resource "docker_container" "trivy" {
-    name = "trivy"
-    image = docker_image.trivy.name
-}
-
-module "docker_host {
+module "docker_host" {
     source = "./modules/docker_host"
-    instance_type = var.instance_type
-    juice_shop_image = var.juice_shop_image
-    zap_image = var.zap_image
+    key_name = var.key_name
+    my_ip = var.my_ip
+    subnet_id = var.subnet_id
+    vpc_id = var.vpc_id
+    ami_id = var.ami_id
 }
