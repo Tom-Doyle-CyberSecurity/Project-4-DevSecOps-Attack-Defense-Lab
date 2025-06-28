251baadb9e55d0e737861ba40f2cda075ea98fd3
Tom-Doyle-CyberSecurity <133623349+Tom-Doyle-CyberSecurity@users.noreply.github.com>
2025-06-28 11:20:46 +0000

    Cleaned syntax & restructured

--- terraform/root_outputs.tf
+++ terraform/root_outputs.tf
@@ -1,19 +1,19 @@
+output "docker_host_ssh_command" {
+    description = "SSH command to connect to the Docker Host"
+    value = module.docker_host.docker_host_ssh_command
+}
+
+output "docker_host_public_ip" {
+    description = "Public IP of the Docker Host"
+    value = module.docker_host.docker_host_public_ip
+}
+
 output "juice_shop_url" {
     description = "Access URL for Juice shop"
-    value  = "http://localhost:${var.juice_shop_port}"
+    value  = "http://${module.docker_host.docker_host_public_ip}:3000"
 }
 
 output "zap_url" {
     description = "Access URL for ZAP"
-    value = "http://localhost:${var.zap_port}"
-}
-
-output "containers" {
-    description = "Deployed container names"
-    value = [
-        docker_container.juice_shop.name,
-        docker_container.zap.name,
-        docker_container.bandit.name,
-        docker_container.trivy.name
-    ]
+    value = "http://${module.docker_host.docker_host_public_ip}:8080"
 }
--- terraform/root_variables.tf
+++ terraform/root_variables.tf
@@ -1,35 +1,47 @@
-## Define variables
+## Define image variables
 
 variable "juice_shop_image" {
-    description = "juice_shop_image"
+    description = "Juice Shop Docker image"
+    type = string
     default = "bkimminich/juice_shop"
 }
 
 variable "zap_image" {
     description = "ZAP Scanner Docker image"
-    default = "bkimminich/juice-shop"
+    type = string
+    default = "owasp/zap2docker-stable"
 }
 
 variable "bandit_image" {
     description = "Python image with Bandit installed"
+    type = string
     default = "python:3.10-slim"
 }
 
 variable "trivy_image" {
     description = "Trivy image vulnerability scanner"
+    type = string
     default = "aquasec/trivy"
 }
 
-variable "instance_type" {}
-variable "juice_shop_image" {}
-variable "zap_image" {}
+## EC2 instance type
 
-## Define ports 
+variable "instance_type" {
+    description = "The type of EC2 instance to use"
+    type = string
+    default = "t2.micro"
+}
+
+## Port variables
 
 variable "juice_shop_port" {
+    description = "Port Juice Shop will be exposed on"
+    type = number
     default = 3000
 }
 
 variable "zap_port" {
+    description = "Port ZAP Scanner will be exposed on"
+    type = number
     default = 8080
 }
